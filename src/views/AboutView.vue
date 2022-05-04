da1d26483e3ce171a5719e2cf2921a4e810cb677
Miguel Huayllas <huayllasmiguel@gmail.com>
2022-05-04 23:34:01 +0200

    Bootstrap version

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<div class="text-center my-3 about">
+  <b-button v-b-tooltip.hover title="Tooltip directive content">
+    Hover Me
+  </b-button>
+
+  <b-button id="tooltip-target-1">
+    Hover Me
+  </b-button>
+  <b-tooltip target="tooltip-target-1" triggers="hover">
+    I am tooltip <b>component</b> content!
+  </b-tooltip>
+</div>
 </template>
